258348940e0ac6a7993f0da5324b310f9b6c25f9
Jiahuan Yang <jiahuan.yang@thoughtworks.com>
2020-02-16 17:17:35 +0800

    [Fibonacci] extract number one and number two

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -8,12 +8,14 @@ public class Fibonacci {
     public int calculate(int number) {
         int minNumber = 50;
         int maxNumber = 1;
+        int initialNumOne = 1;
+        int initialNumTwo = 2;
 
         if (number < maxNumber || number > minNumber) {
             throw new NumberOutofRangeException(String.format("number %d out of range.", number));
         }
 
-        if (number == 1 || number == 2) {
+        if (number == initialNumOne || number == initialNumTwo) {
             return 1;
         } else {
             return this.calculate(number - 1) + this.calculate(number - 2);
