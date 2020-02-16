bdd24717222051cc55bac0c3718019ba06732ce4
Jiahuan Yang <jiahuan.yang@thoughtworks.com>
2020-02-16 17:36:45 +0800

    [Fibonacci] use dynamic programming instead of recursion

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -10,6 +10,8 @@ public class Fibonacci {
         int maxNumber = 1;
         int initialNumOne = 1;
         int initialNumTwo = 2;
+        long[] dpArray = new long[] {1, 1};
+        long result = 0;
 
         if (number < maxNumber || number > minNumber) {
             throw new NumberOutofRangeException(String.format("number %d out of range.", number));
@@ -18,7 +20,12 @@ public class Fibonacci {
         if (number == initialNumOne || number == initialNumTwo) {
             return 1;
         } else {
-            return this.calculate(number - 1) + this.calculate(number - 2);
+            for (int i = initialNumTwo; i < number; i++) {
+                result = dpArray[0] + dpArray[1];
+                dpArray[0] = dpArray[1];
+                dpArray[1] = result;
+            }
+            return result;
         }
     }
 }
